99d48bd6db28e0468dfc1d76e4d2ec07f5534171
M-aminPC <muhammadamin0598@gmail.com>
2018-12-04 13:56:28 +0700

    untuk backup

---
+++ app/src/main/java/com/example/m_aminpc/labinformatika/SplashScreen/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.m_aminpc.labinformatika.SplashScreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.m_aminpc.labinformatika.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, WelcomeActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1500);
+    }
+}
